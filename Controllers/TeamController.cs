480803a69aa4ad68ac7d2f8f03fa227db0ad576e
MarkOhnsman <mark.ohnsman@gmail.com>
2019-07-18 11:19:28 -0600

    Crystal

--- Controllers/TeamController.cs
+++ Controllers/TeamController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using canadiansportsball.Models;
+using canadiansportsball.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace canadiansportsball.Controllers
@@ -10,36 +12,80 @@ namespace canadiansportsball.Controllers
     [ApiController]
     public class TeamsController : ControllerBase
     {
+        private readonly TeamRepository _repo;
+        public TeamsController(TeamRepository repo)
+        {
+            _repo = repo;
+        }
         // GET api/teams
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<Team>> Get()
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                return Ok(_repo.GetAll());
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
         }
 
         // GET api/teams/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<Team> Get(int id)
         {
-            return "value";
+            try
+            {
+                return Ok(_repo.GetById(id));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
         }
 
         // POST api/teams
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult<Team> Post([FromBody] Team value)
         {
+            try
+            {
+                return Ok(_repo.Create(value));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
         }
 
         // PUT api/teams/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public ActionResult<Team> Put(int id, [FromBody] Team value)
         {
+            try
+            {
+                value.Id = id;
+                return Ok(_repo.Update(value));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
         }
 
         // DELETE api/teams/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult<string> Delete(int id)
         {
+            try
+            {
+                return Ok(_repo.Delete(id));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
         }
     }
 }
--- Repositories/TeamsRepository.cs
+++ Repositories/TeamsRepository.cs
@@ -1,7 +1,33 @@
+using System;
+using canadiansportsball.Models;
+
 namespace canadiansportsball.Repositories
 {
     public class TeamRepository
     {
+        internal object GetAll()
+        {
+            throw new NotImplementedException();
+        }
 
+        internal object GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Create(Team value)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Update(Team value)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
