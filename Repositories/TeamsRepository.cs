24aed874e62c6617b8739a9c9aab6706d3b8d941
MarkOhnsman <mark.ohnsman@gmail.com>
2019-07-18 11:28:35 -0600

    Bryce

--- Repositories/TeamsRepository.cs
+++ Repositories/TeamsRepository.cs
@@ -1,31 +1,44 @@
 using System;
+using System.Collections.Generic;
+using System.Data;
 using canadiansportsball.Models;
+using Dapper;
 
 namespace canadiansportsball.Repositories
 {
     public class TeamRepository
     {
-        internal object GetAll()
+        private readonly IDbConnection _db;
+
+        public TeamRepository(IDbConnection db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Team> GetAll()
+        {
+            return _db.Query<Team>("SELECT * FROM teams");
+        }
+
+        public Team GetById(int id)
+        {
+            string query = "SELECT * FROM teams WHERE id = @id";
+            Team team = _db.QueryFirstOrDefault<Team>(query, new { id });
+            if (team == null) throw new Exception("Invalid ID");
+            return team;
+        }
+
+        public Team Create(Team value)
         {
             throw new NotImplementedException();
         }
 
-        internal object GetById(int id)
+        public Team Update(Team value)
         {
             throw new NotImplementedException();
         }
 
-        internal object Create(Team value)
-        {
-            throw new NotImplementedException();
-        }
-
-        internal object Update(Team value)
-        {
-            throw new NotImplementedException();
-        }
-
-        internal object Delete(int id)
+        public object Delete(int id)
         {
             throw new NotImplementedException();
         }
