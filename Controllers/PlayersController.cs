63ae3928666efcaf693b4ce5c4c94b6bc3db2d7e
Dallin Fahnestock <dfahn03@gmail.com>
2019-07-19 16:07:48 -0600

    cleanup

--- Controllers/PlayersController.cs
+++ Controllers/PlayersController.cs
@@ -8,84 +8,84 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace canadiansportsball.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class PlayersController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController]
+  public class PlayersController : ControllerBase
+  {
+    private readonly PlayerRepository _repo;
+    public PlayersController(PlayerRepository repo)
     {
-        private readonly PlayerRepository _repo;
-        public PlayersController(PlayerRepository repo)
-        {
-            _repo = repo;
-        }
-        // GET api/teams
-        [HttpGet]
-        public ActionResult<IEnumerable<Player>> Get()
-        {
-            try
-            {
-                return Ok(_repo.GetAll());
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e);
-            }
-        }
-
-        // GET api/teams/5
-        [HttpGet("{id}")]
-        public ActionResult<Player> Get(int id)
-        {
-            try
-            {
-                return Ok(_repo.GetById(id));
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e);
-            }
-        }
-
-        // POST api/teams
-        [HttpPost]
-        public ActionResult<Player> Post([FromBody] Player value)
-        {
-            try
-            {
-                return Ok(_repo.Create(value));
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e);
-            }
-        }
-
-        // PUT api/teams/5
-        [HttpPut("{id}")]
-        public ActionResult<Player> Put(int id, [FromBody] Player value)
-        {
-            try
-            {
-                value.Id = id;
-                return Ok(_repo.Update(value));
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e);
-            }
-        }
-
-        // DELETE api/teams/5
-        [HttpDelete("{id}")]
-        public ActionResult<string> Delete(int id)
-        {
-            try
-            {
-                return Ok(_repo.Delete(id));
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e);
-            }
-        }
+      _repo = repo;
     }
+    // GET api/players
+    [HttpGet]
+    public ActionResult<IEnumerable<Player>> Get()
+    {
+      try
+      {
+        return Ok(_repo.GetAll());
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e);
+      }
+    }
+
+    // GET api/players/5
+    [HttpGet("{id}")]
+    public ActionResult<Player> Get(int id)
+    {
+      try
+      {
+        return Ok(_repo.GetById(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e);
+      }
+    }
+
+    // POST api/players
+    [HttpPost]
+    public ActionResult<Player> Post([FromBody] Player value)
+    {
+      try
+      {
+        return Ok(_repo.Create(value));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e);
+      }
+    }
+
+    // PUT api/players/5
+    [HttpPut("{id}")]
+    public ActionResult<Player> Put(int id, [FromBody] Player value)
+    {
+      try
+      {
+        value.Id = id;
+        return Ok(_repo.Update(value));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e);
+      }
+    }
+
+    // DELETE api/players/5
+    [HttpDelete("{id}")]
+    public ActionResult<string> Delete(int id)
+    {
+      try
+      {
+        return Ok(_repo.Delete(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e);
+      }
+    }
+  }
 }
